7529b203eb41e2b7206edfe8344f736a820904e0
vaibhavi175-dasv <vaibhavid.ec23@rvce.edu.in>
2026-05-08 14:49:48 +0530

    Refactor AES pipeline stages and key handling

--- AES/aeslite_pipeline.v
+++ AES/aeslite_pipeline.v
@@ -16,45 +16,67 @@ key_expansion_6round ke (
     .round_keys(round_keys)
 );
 
-// Use first expanded key (rk1)
-wire [127:0] round_key1;
-assign round_key1 = round_keys[128 +: 128];
+wire [127:0] rk0 = round_keys[   0 +: 128];
+wire [127:0] rk1 = round_keys[ 128 +: 128];
+wire [127:0] rk2 = round_keys[ 256 +: 128];
+wire [127:0] rk3 = round_keys[ 384 +: 128];
+wire [127:0] rk4 = round_keys[ 512 +: 128];
+wire [127:0] rk5 = round_keys[ 640 +: 128];
+wire [127:0] rk6 = round_keys[ 768 +: 128];
 
-// ===== PIPELINE REGISTERS =====
-reg [127:0] stage1, stage2, stage3, stage4;
+// ===== PIPELINE STAGES =====
+reg [127:0] stage0, stage1, stage2, stage3, stage4, stage5, stage6;
 
-// Stage1
-always @(posedge clk or posedge rst)
-    if (rst) stage1 <= 0;
-    else stage1 <= plaintext ^ key;
+wire [127:0] sb0, sr0, mc0;
+wire [127:0] sb1, sr1, mc1;
+wire [127:0] sb2, sr2, mc2;
+wire [127:0] sb3, sr3, mc3;
+wire [127:0] sb4, sr4, mc4;
+wire [127:0] sb5, sr5;
 
-// Stage2
-wire [127:0] sb_out;
-subbytes sb (.in(stage1), .out(sb_out));
+subbytes sb0_inst (.in(stage0), .out(sb0));
+shiftrows sr0_inst (.in(sb0), .out(sr0));
+mixcolumns mc0_inst (.in(sr0), .out(mc0));
 
-always @(posedge clk or posedge rst)
-    if (rst) stage2 <= 0;
-    else stage2 <= sb_out;
+subbytes sb1_inst (.in(stage1), .out(sb1));
+shiftrows sr1_inst (.in(sb1), .out(sr1));
+mixcolumns mc1_inst (.in(sr1), .out(mc1));
 
-// Stage3
-wire [127:0] sr_out;
-shiftrows sr (.in(stage2), .out(sr_out));
+subbytes sb2_inst (.in(stage2), .out(sb2));
+shiftrows sr2_inst (.in(sb2), .out(sr2));
+mixcolumns mc2_inst (.in(sr2), .out(mc2));
 
-always @(posedge clk or posedge rst)
-    if (rst) stage3 <= 0;
-    else stage3 <= sr_out;
+subbytes sb3_inst (.in(stage3), .out(sb3));
+shiftrows sr3_inst (.in(sb3), .out(sr3));
+mixcolumns mc3_inst (.in(sr3), .out(mc3));
 
-// Stage4
-wire [127:0] mc_out;
-mixcolumns mc (.in(stage3), .out(mc_out));
+subbytes sb4_inst (.in(stage4), .out(sb4));
+shiftrows sr4_inst (.in(sb4), .out(sr4));
+mixcolumns mc4_inst (.in(sr4), .out(mc4));
 
-always @(posedge clk or posedge rst)
-    if (rst) stage4 <= 0;
-    else stage4 <= mc_out;
+subbytes sb5_inst (.in(stage5), .out(sb5));
+shiftrows sr5_inst (.in(sb5), .out(sr5));
 
-// Final
-always @(posedge clk or posedge rst)
-    if (rst) ciphertext <= 0;
-    else ciphertext <= stage4 ^ round_key1;
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        stage0 <= 0;
+        stage1 <= 0;
+        stage2 <= 0;
+        stage3 <= 0;
+        stage4 <= 0;
+        stage5 <= 0;
+        stage6 <= 0;
+        ciphertext <= 0;
+    end else begin
+        stage0 <= plaintext ^ rk0;
+        stage1 <= mc0 ^ rk1;
+        stage2 <= mc1 ^ rk2;
+        stage3 <= mc2 ^ rk3;
+        stage4 <= mc3 ^ rk4;
+        stage5 <= mc4 ^ rk5;
+        stage6 <= sr5 ^ rk6;  // final round: SubBytes + ShiftRows + AddRoundKey
+        ciphertext <= stage6;
+    end
+end
 
 endmodule
